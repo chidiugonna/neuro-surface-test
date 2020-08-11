9d281361f1f902a451f2cc0da17799f57ff75033
Chidi Ugonna <chidi_ugonna@hotmail.com>
2020-08-11 13:31:30 -0700

    tidy file

--- gitcommit.sh
+++ gitcommit.sh
@@ -3,7 +3,5 @@ if [ -z ${1+x} ]
 then 
     echo  "Please pass comments for Git Commit as input parameter. Commit aborted."
 else
-    #gitrepo=$(cat .git/config | grep url | awk -F "= " {'print $2'})
     git commit -m "$1"
-    #git push $gitrepo.git master
 fi
