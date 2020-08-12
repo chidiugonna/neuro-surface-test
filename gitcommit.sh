ca820f3046555729e0bf8ff1b80ab96f39bf93de
Chidi Ugonna <chidi_ugonna@hotmail.com>
2020-08-12 12:25:48 -0700

    deleted files

--- gitcommit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -z ${1+x} ]
-then 
-    echo  "Please pass comments for Git Commit as input parameter. Commit aborted."
-else
-    git commit -m "$1"
-fi
--- gitpush.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
- gitrepo=$(cat .git/config | grep url | awk -F "= " {'print $2'})
- git push $gitrepo.git master
